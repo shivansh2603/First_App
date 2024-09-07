47574ce9b0ca47927505f14162b0438a312c7123
Shivansh Prasad <prasadshivansh66@gmail.com>
2024-09-07 17:36:26 +0530

    Commit 7

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,75 +1,44 @@
 package com.example.loginpage
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
 
-@Suppress("SameParameterValue")
 class MainActivity : AppCompatActivity() {
     private lateinit var emailLayout: TextInputLayout
     private lateinit var passwordLayout: TextInputLayout
-    private lateinit var editTextEmail: EditText
-    private lateinit var editTextPassword: EditText
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         emailLayout = findViewById(R.id.fillEmailLayout)
         passwordLayout = findViewById(R.id.fillPasswordLayout)
-        editTextEmail = findViewById(R.id.fillEmail)
-        editTextPassword = findViewById(R.id.fillPassword)
 
-        val buttonLogin = findViewById<Button>(R.id.pressButton)
-        val backButton = findViewById<ImageButton>(R.id.backButton)
-        backButton.setOnClickListener {
-            finish()
-        }
-        val forgotPasswordButton = findViewById<TextView>(R.id.forgotPasswordButton)
+
+        val forgotPasswordButton = findViewById<TextView>(R.id.registerTextView)
         forgotPasswordButton.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
-        buttonLogin.setOnClickListener {
-            if (validateLayout()) {
-                // Login can be done in the app
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(
-                    this, "Please fill incorrect credentials", Toast.LENGTH_SHORT
-                )
-                    .show()
-            }
-        }
     }
 
     private fun validateLayout(): Boolean {
-        val email = editTextEmail.text.toString()
-        val password = editTextPassword.text.toString()
+        val email = emailLayout.editText?.text.toString()
+        val password = passwordLayout.editText?.text.toString()
 
         emailLayout.error = null
         passwordLayout.error = null
 
-        mutableListOf<String>()
-
-        if (! android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() ||
-            password.length < 6
-        ) {
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() || password.length < 6) {
             showAlertBox("Please check your input and try again.")
             return false
         }
         return true
-        //sare validation passed
     }
 
     private fun showAlertBox(errorMessages: String) {
